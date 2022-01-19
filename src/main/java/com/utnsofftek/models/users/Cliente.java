148d807cfd4329b4b132159c7280701dbd052640
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-01-19 01:15:56 -0300

    fix Cliente para poder usar las ordenes

--- src/main/java/com/utnsofftek/models/users/Cliente.java
+++ src/main/java/com/utnsofftek/models/users/Cliente.java
@@ -1,19 +1,22 @@
 package com.utnsofftek.models.users;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.utnsofftek.models.Orden;
 import com.utnsofftek.models.enumerators.TipoCliente;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @DiscriminatorValue("cliente")
 @Table(name="user_cliente")
@@ -29,5 +32,38 @@ public class Cliente extends Usuario{
 	@Enumerated(EnumType.STRING)
 	@Column(name="tipo_cliente")
 	public TipoCliente tipoCliente;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
+	List<Orden> listaOrdenes;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public TipoCliente getTipoCliente() {
+		return tipoCliente;
+	}
+	public void setTipoCliente(TipoCliente tipoCliente) {
+		this.tipoCliente = tipoCliente;
+	}
+	
 	
 }
